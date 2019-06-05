d8566f86a80df813bd5008edc8eab6012cbc8033
Apple <apple@Apples-MacBook-Pro-6.local>
2019-06-05 16:22:51 -0700

    SecondCommit

--- Aviatrix.swift
+++ Aviatrix.swift
@@ -10,23 +10,37 @@ import Foundation
 
 class Aviatrix {
     
-    func start() -> Bool {
-        return true
+    var authorName = ""
+    var running = false
+    var currentLocation = "St. Louis"
+    var data = AviatrixData()
+    
+    
+    init(author : String){
+        authorName = author
     }
     
+    func start () -> Bool {
+        running = true
+        return running
+    }
+   
     func refuel() {
         
     }
     
     func flyTo(destination : String) {
-        
+        currentLocation = destination
     }
     
-    func distanceTo(target : String) {
-    
+    func distanceTo(target: String, current: String) -> Int{
+        let distance = AviatrixData()
+        return distance.knownDistances[current]![target]!
+        //return data.knownDistances["St. Louis"]!["Phoenix"]!
     }
     
     func knownDestinations() -> [String] {
-       return ["St. Louis"]
+       return ["St. Louis", "Phoenix", "Denver", "SLC"]
     }
+   
 }
